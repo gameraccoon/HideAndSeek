c367300987e82e4478b29cdab9dd196f44354ea0
Pavel Grebnev <ruwhynot@gmail.com>
2021-04-02 14:10:18 +0200

    Added Assume macro

--- src/Base/CompilerHelpers.h
+++ src/Base/CompilerHelpers.h
@@ -1,5 +1,22 @@
 #pragma once
 
+#define COMPILER_UNKNOWN 0
+#define COMPILER_GCC 1
+#define COMPILER_CLANG 2
+#define COMPILER_MSVC 3
+
+// define USED_COMPILER macro
+#if defined(__clang__)
+	#define USED_COMPILER COMPILER_CLANG
+#elif defined(__GNUC__) || defined(__GNUG__)
+	#define USED_COMPILER COMPILER_GCC
+#elif defined(_MSC_VER)
+	#define USED_COMPILER COMPILER_MSVC
+#else
+	// Other compilers
+	#define USED_COMPILER COMPILER_UNKNOWN
+#endif
+
 // ALMOST_ALWAYS/ALMOST_NEVER (likely/unlikely early support)
 #ifdef __has_cpp_attribute
 	#if __has_cpp_attribute(likely) && !(defined(__GNUC__) && __GNUC__ >= 9 && __GNUC__ < 10)
@@ -40,3 +57,15 @@
 
 // CONSTEXPR_ASSERT (to fail compilation when the condition is not passed in a constexpr function)
 #define CONSTEXPR_ASSERT(cond, msg) ((cond) ? void() : std::abort())
+
+// ASSUME (to give a compiler hint that the condition is true at the point where this macro appears)
+// Note: this is dangerous and may result in undifined behaviour if used not carefully
+#if USED_COMPILER == COMPILER_CLANG
+	#define ASSUME(cond) __builtin_assume((cond))
+#elif USED_COMPILER == COMPILER_GCC
+	#define ASSUME(cond) __builtin_expect((cond), 1)
+#elif USED_COMPILER == COMPILER_MSVC
+	#define ASSUME(cond) __assume((cond))
+#else
+	#define ASSUME(cond)
+#endif
