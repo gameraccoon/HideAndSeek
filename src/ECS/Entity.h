de34da9245fd0e3508215b010d5b35b3973d8b8f
Pavel Grebnev <ruwhynot@gmail.com>
2021-05-09 21:52:28 +0200

    Moved SystemManager and Entity methods to the header

--- src/ECS/Entity.h
+++ src/ECS/Entity.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "ErrorHandling.h"
+
 namespace Ecs
 {
 	class Entity
@@ -29,11 +31,34 @@ namespace Ecs
 		explicit OptionalEntity(Entity::EntityId id) : mId(id), mIsValid(true) {}
 
 		[[nodiscard]] bool isValid() const { return mIsValid; }
-		[[nodiscard]] Entity getEntity() const noexcept;
-		[[nodiscard]] Entity::EntityId getId() const noexcept;
+
+		[[nodiscard]] Entity getEntity() const noexcept {
+#ifdef ECS_DEBUG_CHECKS_ENABLED
+			if (!mIsValid)
+			{
+				gErrorHandler("Getting uninitialized entity");
+			}
+#endif // ECS_DEBUG_CHECKS_ENABLED
+			return Entity(mId);
+		}
+
+		[[nodiscard]] Entity::EntityId getId() const noexcept
+		{
+#ifdef ECS_DEBUG_CHECKS_ENABLED
+			if (!mIsValid)
+			{
+				gErrorHandler("Getting uninitialized entity");
+			}
+#endif // ECS_DEBUG_CHECKS_ENABLED
+			return mId;
+		}
 
 	private:
 		Entity::EntityId mId = 0;
 		bool mIsValid = false;
 	};
+
+	static_assert(sizeof(Entity) == sizeof(unsigned int), "Entity is too big");
+	static_assert(std::is_trivially_copyable<Entity>(), "Entity should be trivially copyable");
+	static_assert(std::is_trivially_copyable<OptionalEntity>(), "OptionalEntity should be trivially copyable");
 } // namespace Ecs
--- src/ECS/SystemsManager.h
+++ src/ECS/SystemsManager.h
@@ -26,17 +26,73 @@ namespace Ecs
 			mSystems.emplace_back(new T(std::forward<Args>(args)...));
 		}
 
-		void update();
-		void initResources();
-		void shutdown();
+		void update()
+		{
+#ifdef PROFILE_SYSTEMS
+			mThisFrameTime.frameTime = std::chrono::microseconds::zero();
+			mThisFrameTime.systemsTime.clear();
+#endif // PROFILE_SYSTEMS
+
+			for (std::unique_ptr<System>& system : mSystems)
+			{
+#ifdef PROFILE_SYSTEMS
+				std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
+#endif // PROFILE_SYSTEMS
+
+				// real work is being done here
+				system->update();
 
 #ifdef PROFILE_SYSTEMS
-		SystemsFrameTime getPreviousFrameTimeData();
+				std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now();
+				auto timeDiff = std::chrono::duration_cast<std::chrono::microseconds>(end - start);
+				mThisFrameTime.frameTime += timeDiff;
+				mThisFrameTime.systemsTime.push_back(timeDiff);
 #endif // PROFILE_SYSTEMS
-		std::vector<std::string> getSystemNames();
+			}
+
+#ifdef PROFILE_SYSTEMS
+			mPreviousFrameTime = mThisFrameTime;
+#endif // PROFILE_SYSTEMS
+		}
+
+		void initResources()
+		{
+			for (std::unique_ptr<System>& system : mSystems)
+			{
+				system->initResources();
+			}
+		}
+
+		void shutdown()
+		{
+			for (std::unique_ptr<System>& system : mSystems)
+			{
+				system->shutdown();
+			}
+			mSystems.clear();
+		}
+
+#ifdef PROFILE_SYSTEMS
+	SystemsFrameTime SystemsManager::getPreviousFrameTimeData()
+	{
+		return mPreviousFrameTime;
+	}
+#endif // PROFILE_SYSTEMS
+
+	std::vector<std::string> getSystemNames()
+	{
+		std::vector<std::string> result;
+		result.reserve(mSystems.size());
+		for (std::unique_ptr<System>& system : mSystems)
+		{
+			result.push_back(system->getName());
+		}
+		return result;
+	}
 
 	private:
 		std::vector<std::unique_ptr<System>> mSystems;
+
 #ifdef PROFILE_SYSTEMS
 		SystemsFrameTime mThisFrameTime;
 		SystemsFrameTime mPreviousFrameTime;
