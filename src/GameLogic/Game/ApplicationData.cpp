a0d7964a4c7b53710e24ccee4a5b5a1e7e80e1d0
Pavel Grebnev <ruwhynot@gmail.com>
2022-04-03 21:44:00 +0200

    Move ApplicationData functions implementation to *.cpp

--- src/GameLogic/Game/ApplicationData.cpp
+++ src/GameLogic/Game/ApplicationData.cpp
@@ -1,3 +1,67 @@
 #include "Base/precomp.h"
 
 #include "GameLogic/Game/ApplicationData.h"
+
+#ifdef ENABLE_SCOPED_PROFILER
+#include "Utils/Profiling/ProfileDataWriter.h"
+#endif // ENABLE_SCOPED_PROFILER
+
+#include "GameLogic/Game/HapGame.h"
+
+ApplicationData::ApplicationData(int threadsCount)
+	: WorkerThreadsCount(threadsCount)
+	, RenderThreadId(threadsCount + 1)
+	, threadPool(threadsCount, [this]{ workingThreadSaveProfileData(); })
+{
+}
+
+void ApplicationData::writeProfilingData()
+{
+#ifdef ENABLE_SCOPED_PROFILER
+	{
+		ProfileDataWriter::ProfileData data;
+		{
+			data.scopedProfilerDatas.emplace_back();
+			ProfileDataWriter::ScopedProfilerData& renderScopedProfilerData = data.scopedProfilerDatas.back();
+			renderScopedProfilerData.records = renderThread.getAccessor().consumeScopedProfilerRecordsUnsafe();
+			renderScopedProfilerData.threadId = RenderThreadId;
+		}
+		{
+			data.scopedProfilerDatas.emplace_back();
+			ProfileDataWriter::ScopedProfilerData& mainScopedProfilerData = data.scopedProfilerDatas.back();
+			mainScopedProfilerData.records = gtlScopedProfilerData.getAllRecords();
+			mainScopedProfilerData.threadId = MainThreadId;
+		}
+
+		for (auto&& [threadId, records] : mScopedProfileRecords)
+		{
+			data.scopedProfilerDatas.emplace_back(threadId, std::move(records));
+		}
+
+		data.threadNames.resize(RenderThreadId + 1);
+		data.threadNames[MainThreadId] = "Main Thread";
+		data.threadNames[RenderThreadId] = "Render Thread";
+		for (int i = 0; i < WorkerThreadsCount; ++i)
+		{
+			// zero is reserved for main thread
+			data.threadNames[1 + i] = std::string("Worker Thread #") + std::to_string(i+1);
+		}
+
+		ProfileDataWriter::PrintScopedProfileToFile(ScopedProfileOutputPath, data);
+	}
+#endif // ENABLE_SCOPED_PROFILER
+}
+
+void ApplicationData::workingThreadSaveProfileData()
+{
+#ifdef ENABLE_SCOPED_PROFILER
+	std::lock_guard l(mScopedProfileRecordsMutex);
+	mScopedProfileRecords.emplace_back(ThreadPool::GetThisThreadId(), gtlScopedProfilerData.getAllRecords());
+#endif // ENABLE_SCOPED_PROFILER
+}
+
+void ApplicationData::shutdownThreads()
+{
+	threadPool.shutdown();
+	renderThread.shutdownThread();
+}
--- src/GameLogic/Game/ApplicationData.h
+++ src/GameLogic/Game/ApplicationData.h
@@ -3,11 +3,11 @@
 #include "HAL/Base/Engine.h"
 
 #include "Utils/Multithreading/ThreadPool.h"
-#ifdef ENABLE_SCOPED_PROFILER
-#include "Utils/Profiling/ProfileDataWriter.h"
-#endif // ENABLE_SCOPED_PROFILER
 
 #include "GameLogic/Render/RenderThreadManager.h"
+
+class ArgumentsParser;
+class ResourceManager;
 
 class ApplicationData
 {
@@ -22,63 +22,11 @@ public:
 	RenderThreadManager renderThread;
 
 public:
-	ApplicationData(int threadsCount)
-		: WorkerThreadsCount(threadsCount)
-		, RenderThreadId(threadsCount + 1)
-		, threadPool(threadsCount, [this]{ workingThreadSaveProfileData(); })
-	{
-	}
+	ApplicationData(int threadsCount);
 
-	void writeProfilingData()
-	{
-#ifdef ENABLE_SCOPED_PROFILER
-		{
-			ProfileDataWriter::ProfileData data;
-			{
-				data.scopedProfilerDatas.emplace_back();
-				ProfileDataWriter::ScopedProfilerData& renderScopedProfilerData = data.scopedProfilerDatas.back();
-				renderScopedProfilerData.records = renderThread.getAccessor().consumeScopedProfilerRecordsUnsafe();
-				renderScopedProfilerData.threadId = RenderThreadId;
-			}
-			{
-				data.scopedProfilerDatas.emplace_back();
-				ProfileDataWriter::ScopedProfilerData& mainScopedProfilerData = data.scopedProfilerDatas.back();
-				mainScopedProfilerData.records = gtlScopedProfilerData.getAllRecords();
-				mainScopedProfilerData.threadId = MainThreadId;
-			}
-
-			for (auto&& [threadId, records] : mScopedProfileRecords)
-			{
-				data.scopedProfilerDatas.emplace_back(threadId, std::move(records));
-			}
-
-			data.threadNames.resize(RenderThreadId + 1);
-			data.threadNames[MainThreadId] = "Main Thread";
-			data.threadNames[RenderThreadId] = "Render Thread";
-			for (int i = 0; i < WorkerThreadsCount; ++i)
-			{
-				// zero is reserved for main thread
-				data.threadNames[1 + i] = std::string("Worker Thread #") + std::to_string(i+1);
-			}
-
-			ProfileDataWriter::PrintScopedProfileToFile(ScopedProfileOutputPath, data);
-		}
-#endif // ENABLE_SCOPED_PROFILER
-	}
-
-	void workingThreadSaveProfileData()
-	{
-#ifdef ENABLE_SCOPED_PROFILER
-		std::lock_guard l(mScopedProfileRecordsMutex);
-		mScopedProfileRecords.emplace_back(ThreadPool::GetThisThreadId(), gtlScopedProfilerData.getAllRecords());
-#endif // ENABLE_SCOPED_PROFILER
-	}
-
-	void shutdownThreads()
-	{
-		threadPool.shutdown();
-		renderThread.shutdownThread();
-	}
+	void writeProfilingData();
+	void workingThreadSaveProfileData();
+	void shutdownThreads();
 
 private:
 	std::vector<std::pair<size_t, ScopedProfilerThreadData::Records>> mScopedProfileRecords;
